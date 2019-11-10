163aea7be768d3f17f7fce4fdddb81b6982a8cec
Will Thames <will@thames.id.au>
2019-11-10 17:19:33 +1000

    Initial work on proof of concept for openpolicyagent tracing

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/willthames/opa-tracing
+
+require github.com/open-policy-agent/opa v0.15.0
+
+require (
+	github.com/Sirupsen/logrus v1.0.3
+	github.com/apache/thrift v0.0.0-20161221203622-b2a4d4ae21c7 // indirect
+	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/facebookgo/inject v0.0.0-20180706035515-f23751cae28b // indirect
+	github.com/facebookgo/limitgroup v0.0.0-20150612190941-6abd8d71ec01 // indirect
+	github.com/facebookgo/muster v0.0.0-20150708232844-fd3d7953fd52 // indirect
+	github.com/facebookgo/startstop v0.0.0-20161013234910-bc158412526d // indirect
+	github.com/facebookgo/structtag v0.0.0-20150214074306-217e25fb9691 // indirect
+	github.com/honeycombio/honeycomb-opentracing-proxy v1.0.1
+	github.com/honeycombio/libhoney-go v1.12.2 // indirect
+	github.com/klauspost/compress v1.9.1 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/uber/jaeger v1.15.1 // indirect
+	github.com/uber/tchannel-go v1.16.0 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	go.uber.org/atomic v1.5.0 // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/alexcesaro/statsd.v2 v2.0.0 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+)
+
+go 1.13
---
+++ server.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/honeycombio/honeycomb-opentracing-proxy/app"
+	"github.com/honeycombio/honeycomb-opentracing-proxy/types"
+)
+
+type App struct {
+	Port   string
+	server *http.Server
+}
+
+// handleSpans handles the /api/v1/spans POST endpoint. It decodes the request
+// body and normalizes it to a slice of types.Span instances. The Sink
+// handles that slice. The Mirror, if configured, takes the request body
+// verbatim and sends it to another host.
+func (a *App) handleSpans(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		logrus.WithError(err).Info("Error reading request body")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error reading request"))
+	}
+
+	contentType := r.Header.Get("Content-Type")
+
+	var spans []*types.Span
+	switch contentType {
+	case "application/json":
+		logrus.Info("Receiving data in json format")
+		spans, err = types.DecodeJSON(bytes.NewReader(data))
+	case "application/x-thrift":
+		logrus.Info("Receiving data in thrift format")
+		spans, err = types.DecodeThrift(bytes.NewReader(data))
+	default:
+		logrus.WithField("contentType", contentType).Info("unknown content type")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unknown content type"))
+		return
+	}
+	if err != nil {
+		logrus.WithError(err).WithField("type", contentType).Info("error unmarshaling spans")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error unmarshaling span data"))
+		return
+	}
+
+	for _, span := range spans {
+		logrus.WithField("span", span).Info("Accepted span")
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+}
+
+// ungzipWrap wraps a handleFunc and transparently ungzips the body of the
+// request if it is gzipped
+func ungzipWrap(hf func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var newBody io.ReadCloser
+		isGzipped := r.Header.Get("Content-Encoding")
+		if isGzipped == "gzip" {
+			buf := bytes.Buffer{}
+			if _, err := io.Copy(&buf, r.Body); err != nil {
+				logrus.WithError(err).Info("error allocating buffer for ungzipping")
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("error allocating buffer for ungzipping"))
+				return
+			}
+			var err error
+			newBody, err = gzip.NewReader(&buf)
+			if err != nil {
+				logrus.WithError(err).Info("error ungzipping span data")
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("error ungzipping span data"))
+				return
+			}
+			r.Body = newBody
+		}
+		hf(w, r)
+	}
+}
+
+func (a *App) Start() error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/spans", ungzipWrap(a.handleSpans))
+
+	a.server = &http.Server{
+		Addr:    a.Port,
+		Handler: mux,
+	}
+	go a.server.ListenAndServe()
+	logrus.WithField("port", a.Port).Info("Listening")
+	return nil
+}
+
+func (a *App) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	return a.server.Shutdown(ctx)
+}
+
+func main() {
+	a := &app.App{
+		Port: "8080",
+	}
+	err := a.Start()
+	if err != nil {
+		fmt.Printf("Error starting app: %v\n", err)
+		os.Exit(1)
+	}
+	defer a.Stop()
+	waitForSignal()
+}
+
+func waitForSignal() {
+	ch := make(chan os.Signal, 1)
+	defer close(ch)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(ch)
+	<-ch
+}
