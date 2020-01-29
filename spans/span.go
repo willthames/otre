f63d1f99ce19bfb991ea7c6513251eb78425df74
Will Thames <will@thames.id.au>
2020-01-29 13:08:39 +1000

    Thrift improvements

--- spans/span.go
+++ spans/span.go
@@ -3,32 +3,39 @@ package spans
 import (
 	"encoding/json"
 	"time"
+
+	"github.com/sirupsen/logrus"
+	thrift "github.com/thrift-iterator/go"
 )
 
 // Span represents the Zipkin V1 Span object. See
 // https://github.com/openzipkin/zipkin-api/blob/master/zipkin-api.yaml
 type Span struct {
-	TraceID           string                 `thrift:"trace_id,1" json:"traceId"`
-	Name              string                 `thrift:"name,3" json:"name"`
-	ID                string                 `thrift:"id,4" json:"id"`
-	ParentID          string                 `thrift:"parent_id,5" json:"parentId,omitempty"`
-	Annotations       []*annotation          `thrift:"annotations,6" json:"annotations"`
-	BinaryAnnotations map[string]interface{} `thrift:"-" json:"-"`
-	Debug             bool                   `thrift:"debug,9" json:"debug,omitempty"`
-	Timestamp         time.Time              `thrift:"-" json:"-"`
-	Duration          time.Duration          `thrift:"-" json:"-"`
-	TraceIDHigh       *int64                 `thrift:"trace_id_high,12" json:"trace_id_high,omitempty"`
+	TraceID           string
+	Name              string
+	ID                string
+	ParentID          string
+	Annotations       []*annotation
+	BinaryAnnotations map[string]string
+	Debug             bool
+	Timestamp         time.Time
+	Duration          time.Duration
+	TraceIDHigh       *int64
 }
 
-// JSONSpan is used as an intermediate step between json encoding and the Span tyhpe
-type JSONSpan struct {
-	spanAlias
-	BinaryAnnotations []BinaryAnnotation `thrift:"binary_annotations,8" json:"binaryAnnotations"`
+// v1Span is used as an intermediate step between encoding and the Span tyhpe
+type v1Span struct {
+	TraceID           string             `thrift:"trace_id,1" json:"traceId"`
+	Name              string             `thrift:"name,3" json:"name"`
+	ID                string             `thrift:"id,4" json:"id"`
+	ParentID          string             `thrift:"parent_id,5" json:"parentId,omitempty"`
+	Annotations       []*annotation      `thrift:"annotations,6" json:"annotations"`
+	Debug             bool               `thrift:"debug,9" json:"debug,omitempty"`
+	TraceIDHigh       *int64             `thrift:"trace_id_high,12" json:"trace_id_high,omitempty"`
+	BinaryAnnotations []BinaryAnnotation `thrift:"binary_annotations,8" json:"binary_annotations"`
 	Timestamp         int64              `thrift:"timestamp,10" json:"timestamp,omitempty"`
 	Duration          int64              `thrift:"duration,11" json:"duration,omitempty"`
 }
-
-type spanAlias Span
 
 type annotation struct {
 	timestamp int64     `json:"timestamp"`
@@ -36,40 +43,10 @@ type annotation struct {
 	endpoint  *endpoint `json:"endpoint,omitempty"`
 }
 
-// Span converts a JSONSpan into Span after Unmarshalling
-func (jsonspan JSONSpan) Span() Span {
-	span := Span(jsonspan.spanAlias)
-	span.Duration = time.Duration(jsonspan.Duration * 1E3)
-	span.Timestamp = time.Unix(jsonspan.Timestamp/1E6, (jsonspan.Timestamp%1E6)*1E3)
-	span.BinaryAnnotations = make(map[string]interface{}, len(jsonspan.BinaryAnnotations))
-	for _, annotation := range jsonspan.BinaryAnnotations {
-		span.BinaryAnnotations[annotation.key] = annotation.value
-	}
-	return span
-}
-
-// NewJSONSpan converts a Span into JSONSpan suitable for Marshalling
-func NewJSONSpan(span Span) JSONSpan {
-	return JSONSpan{
-		spanAlias(span),
-		convertAnnotations(span.BinaryAnnotations),
-		span.Timestamp.UnixNano() / 1E3,
-		span.Duration.Microseconds(),
-	}
-}
-
-func convertAnnotations(annotations map[string]interface{}) []BinaryAnnotation {
-	result := make([]BinaryAnnotation, len(annotations))
-	for key, value := range annotations {
-		result = append(result, BinaryAnnotation{key: key, value: value})
-	}
-	return result
-}
-
 type BinaryAnnotation struct {
-	key      string      `json:"key"`
-	value    interface{} `json:"value"`
-	endpoint *endpoint   `json:"endpoint,omitempty"`
+	Key      string    `thrift:"key,13" json:"key"`
+	Value    string    `thrift:"value,14" json:"value"`
+	endpoint *endpoint `json:"endpoint,omitempty"`
 }
 
 type endpoint struct {
@@ -78,15 +55,81 @@ type endpoint struct {
 	serviceName string `json:"serviceName"`
 }
 
+// Span converts a JSONSpan into Span after Unmarshalling
+func (v1span v1Span) Span() Span {
+	span := Span{
+		TraceID:     v1span.TraceID,
+		Name:        v1span.Name,
+		ID:          v1span.ID,
+		ParentID:    v1span.ParentID,
+		Annotations: v1span.Annotations,
+		Debug:       v1span.Debug,
+		TraceIDHigh: v1span.TraceIDHigh,
+	}
+	span.Duration = time.Duration(v1span.Duration * 1E3)
+	span.Timestamp = time.Unix(v1span.Timestamp/1E6, (v1span.Timestamp%1E6)*1E3)
+	span.BinaryAnnotations = make(map[string]string, len(v1span.BinaryAnnotations))
+	for _, annotation := range v1span.BinaryAnnotations {
+		span.BinaryAnnotations[annotation.Key] = annotation.Value
+	}
+	return span
+}
+
+// NewJSONSpan converts a Span into JSONSpan suitable for Marshalling
+func newV1Span(span Span) v1Span {
+	binaryAnnotations := convertAnnotations(span.BinaryAnnotations)
+	timestamp := span.Timestamp.UnixNano() / 1E3
+	duration := span.Duration.Microseconds()
+	v1span := v1Span{
+		TraceID:           span.TraceID,
+		Name:              span.Name,
+		ID:                span.ID,
+		ParentID:          span.ParentID,
+		Annotations:       span.Annotations,
+		Debug:             span.Debug,
+		TraceIDHigh:       span.TraceIDHigh,
+		BinaryAnnotations: binaryAnnotations,
+		Timestamp:         timestamp,
+		Duration:          duration,
+	}
+	return v1span
+}
+
+func convertAnnotations(annotations map[string]string) []BinaryAnnotation {
+	result := make([]BinaryAnnotation, len(annotations))
+	index := 0
+	for key, value := range annotations {
+		result[index] = BinaryAnnotation{Key: key, Value: value}
+		index++
+	}
+	return result
+}
+
 func (s Span) MarshalJSON() ([]byte, error) {
-	return json.Marshal(NewJSONSpan(s))
+	return json.Marshal(newV1Span(s))
 }
 
 func (s *Span) UnmarshalJSON(data []byte) error {
-	var js JSONSpan
-	if err := json.Unmarshal(data, &js); err != nil {
+	var v1span v1Span
+	if err := json.Unmarshal(data, &v1span); err != nil {
 		return err
 	}
-	*s = js.Span()
+	logrus.WithField("span", v1span).Trace("Unmarshalled span from json")
+	*s = v1span.Span()
+	return nil
+}
+
+func (s Span) MarshalThrift() ([]byte, error) {
+	v1span := newV1Span(s)
+	return thrift.Marshal(v1span)
+}
+
+func (s *Span) UnmarshalThrift(data []byte) error {
+	var v1span v1Span
+	if err := thrift.Unmarshal(data, &v1span); err != nil {
+		return err
+	}
+	logrus.WithField("span", v1span).Trace("Unmarshalled span from thrift")
+	*s = v1span.Span()
 	return nil
 }
--- spans/span_test.go
+++ spans/span_test.go
@@ -1,20 +1,40 @@
 package spans
 
 import (
-	"encoding/json"
 	"testing"
 	"time"
 )
 
-func testJSONUnmarshal(t *testing.T) {
-	b := []byte(`{"id":1,"name":"bowser","timestamp":1480979203000000,"binaryAnnotations":[{"key":"hello","value":"world"}],"duration":1000}`)
+func TestJSONUnmarshal(t *testing.T) {
+	b := []byte(`{"id":"hello","name":"bowser","timestamp":1480979203000000,"binary_annotations":[{"key":"hello","value":"world"}],"duration":1000}`)
 	span := new(Span)
-	json.Unmarshal(b, &span)
+	err := span.UnmarshalJSON(b)
+	if err != nil {
+		t.Errorf("Failed to unmarshal span from json: %v", err)
+	}
 	if span.Duration != 1000*time.Microsecond {
 		t.Errorf("span duration was incorrectly parsed")
 	}
 	value, ok := span.BinaryAnnotations["hello"]
 	if !ok || value != "world" {
 		t.Errorf("binary annotations incorrectly parsed")
+	}
+}
+
+func TestThriftMarshall(t *testing.T) {
+	span := Span{ID: "hello", TraceID: "world", Timestamp: time.Now()}
+	span.BinaryAnnotations = make(map[string]string)
+	span.BinaryAnnotations["this"] = "that"
+	b, err := span.MarshalThrift()
+	if err != nil {
+		t.Errorf("Failed to marshal span to thrift")
+	}
+	span2 := new(Span)
+	err = span2.UnmarshalThrift(b)
+	if err != nil {
+		t.Errorf("Failed to unmarshal span from thrift")
+	}
+	if span.ID != span2.ID || span.TraceID != span2.TraceID || span.BinaryAnnotations["this"] != span2.BinaryAnnotations["this"] {
+		t.Errorf("span %v doesn't equal span2 %v", span, span2)
 	}
 }
